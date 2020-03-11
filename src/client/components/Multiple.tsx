2a51c37e7cb9daa878199388bb37da4c9d42775d
Liv <w.phrush@gmail.com>
2020-03-10 21:55:45 -0500

    build basic structure for multiple question content

--- src/client/components/Multiple.tsx
+++ src/client/components/Multiple.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { TQuestion } from '../App';
 
-export const Multiple = ({ content }: { content: TQuestion }) => (
-  <div className="question-multiple">
-    <p>
-      {content.question}
-    </p>
-  </div>
-);
+export const Multiple = ({ content }: { content: TQuestion }) => {
+  const answers = [content.correct_answer].concat(content.incorrect_answers);
+  return (
+    <div className="question-multiple">
+      <h2>{content.question}</h2>
+      {
+        answers.map((answer, index) => (
+          <div className="check" key={answer.slice(0, 5)}>
+            <input type="radio"/>
+            <label htmlFor={`answer-${index}`}>{answer}</label>
+          </div>
+        ))
+      }
+      <button>Next</button>
+    </div>
+  );
+}
