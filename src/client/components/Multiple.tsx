16ce2f6c2052dd4390ca60e4e20454db08535eb4
Liv <w.phrush@gmail.com>
2020-03-10 22:02:27 -0500

    use hooks to track multiple question input state

--- src/client/components/Multiple.tsx
+++ src/client/components/Multiple.tsx
@@ -2,14 +2,22 @@ import * as React from 'react';
 import { TQuestion } from '../App';
 
 export const Multiple = ({ content }: { content: TQuestion }) => {
-  const answers = [content.correct_answer].concat(content.incorrect_answers);
+  const answers: string[] = [content.correct_answer].concat(content.incorrect_answers);
+  const [selectedAnswer, updateSelectedAnswer] = React.useState('');
+
   return (
     <div className="question-multiple">
       <h2>{content.question}</h2>
       {
         answers.map((answer, index) => (
           <div className="check" key={answer.slice(0, 5)}>
-            <input type="radio"/>
+            <input
+              id={`answer-${index}`}
+              type="radio"
+              value={answer}
+              checked={answer === selectedAnswer}
+              onChange={() => updateSelectedAnswer(answer)}
+            />
             <label htmlFor={`answer-${index}`}>{answer}</label>
           </div>
         ))
