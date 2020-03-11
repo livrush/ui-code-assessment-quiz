e691d429fbc618867994aea5fec9a140bad2969b
Liv <w.phrush@gmail.com>
2020-03-10 23:04:04 -0500

    add style file

---
+++ src/client/style.ts
@@ -0,0 +1,10 @@
+import CSS from 'csstype';
+
+export const buttonStyle: CSS.Properties = {
+  background: '#1D70DD',
+  color: '#FFF',
+  padding: '.5rem 1rem',
+  border: 'none',
+  fontSize: '1rem',
+};
+
