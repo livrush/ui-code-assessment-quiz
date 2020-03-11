10ab5236379c8d2d45ecbb905a70be1b36238596
Liv <w.phrush@gmail.com>
2020-03-10 23:04:24 -0500

    add summary component

---
+++ src/client/components/Summary.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { buttonStyle } from '../style';
+
+export const Summary = ({ correct, wrong }: {
+  correct: number, wrong: number,
+}) => {
+  const answered: number = correct + wrong;
+  const score: number = Math.round(correct / answered * 100) || 0;
+
+  return (
+    <div className="summary">
+      <h1>SUMMARY</h1>
+      <p>Correct: <span style={{fontWeight: 'bold'}}>{correct}</span></p>
+      <p>Wrong: <span style={{fontWeight: 'bold'}}>{wrong}</span></p>
+      <p>Questions answered: <span style={{fontWeight: 'bold'}}>{answered}</span></p>
+      <p>Final Score: <span style={{fontWeight: 'bold'}}>{score}%</span></p>
+      <button style={buttonStyle}>Restart Quiz</button>
+    </div>
+  );
+};
