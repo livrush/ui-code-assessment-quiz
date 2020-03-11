34dc0c799256c8fa048c566fecd32b8de5abdfb6
Liv <w.phrush@gmail.com>
2020-03-10 20:34:11 -0500

    refactor App to stateful component, add base state

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 
-export const App = () => (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h1>Lucid</h1>
-        <h2>Welcome to UI Team code assessment!</h2>
-    </div>
-);
+class App extends React.Component<{}> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            questions: [],
+        };
+    }
+
+    render() {
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <h1>Lucid Quiz</h1>
+                <h2>Try your best to answer these questions.</h2>
+            </div>
+        )
+    }
+}
+
+export { App };
