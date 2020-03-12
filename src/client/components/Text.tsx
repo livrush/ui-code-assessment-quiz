81c1160901146d16a25c9d328deadbb0d8f47357
Liv <w.phrush@gmail.com>
2020-03-11 20:22:46 -0500

    reset text answer state when next button clicked

--- src/client/components/Text.tsx
+++ src/client/components/Text.tsx
@@ -17,9 +17,24 @@ export const Text = ({ content, handleNext }: {
     <div className="question-text">
       <h1 style={h1Style} dangerouslySetInnerHTML={{ __html: content.question }}></h1>
       <div className="text-answer-container" style={answerContainerStyle}>
-        <input className="text-answer-input" style={inputStyle} type="text" name="" />
+        <input
+          className="text-answer-input"
+          name="text answer"
+          onChange={(e) => {console.log(e.currentTarget.value); updateSelectedAnswer(e.currentTarget.value);}}
+          style={inputStyle}
+          type="text"
+          value={selectedAnswer}
+        />
       </div>
-      <button style={buttonStyle} onClick={() => handleNext(selectedAnswer, content.correct_answer)}>Next</button>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          handleNext(selectedAnswer, content.correct_answer);
+          updateSelectedAnswer('');
+        }}
+      >
+        Next
+      </button>
     </div>
   );
-}
+};
